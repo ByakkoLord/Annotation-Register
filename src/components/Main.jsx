3ec4386041a174043fed78e92c3771124520ee09
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-05 22:03:50 -0300

    Add Annotations Boxs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,10 @@
 export default function Main() {
     return (
         <main id='app' className='bg-transparent ml-5 w-8/12 rounded-2xl'>
-            <article className="flex flex-col w-44 h-56 bg-gray-100 rounded-2xl">
-                <h4>Titulo</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ea dolor maiores maxime beatae autem</p>
-                <i className="w-72 h-72 fa-solid fa-exclamation"></i>
+            <article className="p-3 flex flex-col w-64 bg-gray-100 rounded-2xl">
+                <h4 className="font-bold text-lg">Titulo</h4>
+                <p className="mt-3 font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ea dolor maiores maxime beatae autem</p>
+                <i className="ml-5 mt-7 text-3xl fa-solid fa-exclamation"></i>
             </article>
                 
             
