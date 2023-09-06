5b9d1bf539e60874938eddb755ebef9025ca98e2
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-05 21:58:29 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Aside from './components/Aside'
+import Main from './components/Main'
+
+function App() {
+
+  return (
+    <div id='app' className='flex items-end justify-center bg-gray-300 w-screen h-screen'>
+      <section className='flex justify-around p-10 bg-gray-400 w-4/5 h-5/6 rounded-2xl shadow-2xl' >
+        <Aside/>
+        <Main/>
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main() {
+    return (
+        <main id='app' className='bg-transparent ml-5 w-8/12 rounded-2xl'>
+            <article className="flex flex-col w-44 h-56 bg-gray-100 rounded-2xl">
+                <h4>Titulo</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ea dolor maiores maxime beatae autem</p>
+                <i className="w-72 h-72 fa-solid fa-exclamation"></i>
+            </article>
+                
+            
+        </main>
+    )
+}
