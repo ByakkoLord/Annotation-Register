2daf70f439244b50f17020d82e457613a5253c67
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-06 17:34:07 -0300

    Add handleSubmit Function and connected with serv

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Aside from './components/Aside'
 import Main from './components/Main'
 
+
 function App() {
 
   return (
---
+++ src/components/Annotations.jsx
@@ -0,0 +1,9 @@
+export default function Annotations() {
+    return (
+        <article className="p-3 flex flex-col w-64 bg-gray-100 rounded-2xl">
+            <h4 className="font-bold text-lg">Titulo</h4>
+            <p className="mt-3 font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ea dolor maiores maxime beatae autem</p>
+            <i className="ml-5 mt-7 text-3xl fa-solid fa-exclamation"></i>
+        </article>
+    )
+}
