5f526449ac8bec8c80b925684744f1e336907621
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-07 15:37:04 -0300

    Adjust Annotations blocks

--- src/components/Annotations.jsx
+++ src/components/Annotations.jsx
@@ -1,12 +1,26 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
+import EraseQuestion from './EraseQuestion'
 
 export default function Annotations({ data }) {
+    const [deleteQuestion, setDeleteQuestion] = useState(false)
+
+        function questionDelete() {
+            setDeleteQuestion(true)
+        }
     return (
-        <article className="p-3 flex flex-col h-56 overflow-y-auto w-64 bg-gray-100 rounded-2xl">
+        <article className="p-3 flex flex-col h-56 w-64 shadow-2xl bg-gray-100 rounded-2xl">
             <h4 className="font-bold text-lg border-b-2 pb-3">{data.title}</h4>
-            <p className="mt-3 font-medium">{data.notes}</p>
-            <i className="ml-5 mt-7 text-3xl fa-solid fa-exclamation"></i>
+            <div className='overflow-y-auto h-full '>
+                <p className="mt-3 font-medium">{data.notes}</p>
+            </div>
+            <div className='flex mt-2 items-center w-full justify-between'>
+                <i className="hover:scale-125 transition-all cursor-pointer ml-5 text-3xl fa-solid fa-exclamation"></i>
+                <i onClick={questionDelete} className="hover:scale-125 transition-all cursor-pointer mr-5 text-xl fa-solid fa-trash"></i>
+            </div>
+            <EraseQuestion display={deleteQuestion}/>
         </article>
+        
     )
 }
 
